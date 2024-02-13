8cc09870c88780fe2491306b717580b74e2f2fb1
Md Akash <akashnazmul649@gmail.com>
2024-02-13 23:29:59 +0600

    HeaderStart.php

---
+++ HeaderStart.php
@@ -0,0 +1,8 @@
+<html lang="<?php language_attributes();?>" class="no-js">
+<head>
+    <meta charset="<?php bloginfo('cherset')?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
