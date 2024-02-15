72372949bf9d77645656cd76857955004b8c4e1e
Md Akash <akashnazmul649@gmail.com>
2024-02-15 16:34:02 +0600

    update

--- nave_rasister & display.php
+++ nave_rasister & display.php
@@ -1,3 +1,4 @@
+<?php
 //resister nav menu
 register_nav_menu('main_menu', __('Main Menu', 'ifmyespts'));
 
@@ -8,3 +9,5 @@ $main_menu = wp_nav_menu(array('theme_location' => 'main_menu', 'menu_id' => 'ma
 $main_menu = wp_nav_menu(array('theme_location' => 'main_menu', 'menu_id' => 'mainMenu', 'menu_class' => 'dskMenu', 'echo' => false));
 $main_menu = str_replace(array('<div class="menu-main-menu-container">', '</div>'), '', $main_menu);
 echo $main_menu;
+
+?>
--- wp_enqueue.php
+++ wp_enqueue.php
@@ -1,3 +1,5 @@
+<?php
+
 function myespt_css_js() {
     // CSS
 	wp_enqueue_style('wpcore', get_template_directory_uri().'/assets/css/wp-core.css');
@@ -5,11 +7,14 @@ function myespt_css_js() {
     	wp_enqueue_style('myespt-style', get_stylesheet_uri());//this is root path style.css 
    
     // JS
-    	wp_enqueue_script('jquery');//this is jaquery
+    	wp_enqueue_script('jquery');//this is jaquery // Note : Sometimes wordpress jquery.js your theme doesn't work, then use your jquery file.
        	wp_enqueue_script('mainJS', get_template_directory_uri().'/assets/js/main.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'myespt_css_js');
 
 //if you want connect your css and js in header.php
 <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/style.css">
+
+
+?>
 
